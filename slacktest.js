6eb28864a280acb4ebc5ba5b73edd3e25e658116
Reid Stidolph <rstidolph@128technology.com>
2016-11-25 09:54:58 -0800

    More ES6 updates and clean up

--- slacktest.js
+++ slacktest.js
@@ -1,5 +1,6 @@
 /*jslint node: true */
 "use strict";
+
 //
 // This NodeJS application that uses a 128T Router and
 // outgoing Slack webhooks, to post router information
@@ -12,20 +13,23 @@
 // ./t128-slackbot-config.json
 //
 
+try {var config = require("./slackbot-config.json");}
+catch(err) {
+	process.stdout.write(`\n${err}\n`);
+    process.stdout.write(`
+        Something is wrong with the 'slackbot-config.json'
+        file. You may not have set it up correctly. Make
+        sure the file exists in the root of this app 
+        directory. See the sample-config.json for an 
+        example 'slackbot-config.json' file.\n\n`
+    );
+    process.exit(1);
+}
 var slack = require("./lib/slack.js");
 var routerConfig = {};
 var t128 = require("./lib/t128-router.js");
 var healthReport = require("./lib/healthReport.js");
 var alarm = require("./lib/alarm.js");
-try	{var config = require("./slackbot-config.json");}
-catch(err) {
-    console.log("\nError with the 'slackbot-config.json' file.");
-    console.log("You may not have set it up correctly.");
-    console.log("Make sure the file exists in the root of this");
-    console.log("app directory. See the sample-config.json for an"); 
-    console.log("example 'slackbot-config.json'. \n\n");
-    process.exit(1);
-}
 
 var testAlarm = require("./templates/testAlarmData.json");
 var alarmOutput = alarm(testAlarm);
