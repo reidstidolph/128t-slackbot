0ee4a5d51130495e85bbfa765927a734afe5c743
Reid Stidolph <rstidolph@128technology.com>
2016-12-18 20:55:08 -0800

    initial logger added

---
+++ test/loggerTest.js
@@ -0,0 +1,19 @@
+/*jshint node: true */
+"use strict";
+
+var Logger =  require("../lib/logger.js");
+var logger = new Logger("loggerTest", "notice");
+
+logger.log("info", "Test 123");
+
+var obj = {"foo" : "fighter"};
+
+logger.log("info", obj);
+
+obj.fi = "new data";
+
+logger.log("debug", "object is:", obj);
+logger.log("info", "object is:", obj);
+logger.log("info", "object is:", obj);
+logger.log("notice", "object is:", obj);
+logger.log("critical", "object is:", obj);
